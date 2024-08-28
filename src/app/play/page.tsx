1c272eed2198d03b89ec0375d0047266717aac9b
Quinn Eschenbach <quinneschenbach@ymail.com>
2024-08-28 21:45:35 +0100

    small hack to fix hydration errors connected to wallets

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+import { useEffect, useState } from "react";
 import GamePlayPageClient from "./client";
 
 export default function GamePlayPage() {
+  // little hack to fix hydration errors connected to wallet stuff
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) {
+    return <></>;
+  }
+
   return <GamePlayPageClient />;
 }
